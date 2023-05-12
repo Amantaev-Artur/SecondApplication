b95ef5dfa21a23cb9c62a07c991c5cdaf0df7efe
Artur Amantaev <amantaev_artur@mail.ru>
2023-05-12 19:19:37 +0300

    gifs

--- app/src/main/java/com/example/secondapplication/datalayer/IItemAccessor2.kt
+++ app/src/main/java/com/example/secondapplication/datalayer/IItemAccessor2.kt
@@ -4,6 +4,7 @@ import com.example.secondapplication.objects.Item
 import retrofit2.http.GET
 
 interface IItemAccessor2 {
-	@GET("/api/cats?skip=0&limit=1000")
+//	API docs: https://cataas.com/cat/rV1MVEh0Af2Bm4O0
+	@GET("/api/cats?skip=0&limit=1000&tags=gif")
 	suspend fun items2(): List<Item>
 }
--- app/src/main/java/com/example/secondapplication/presentationlayer/adapters/ItemViewHolder.kt
+++ app/src/main/java/com/example/secondapplication/presentationlayer/adapters/ItemViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.secondapplication.presentationlayer.adapters
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
@@ -7,26 +8,27 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.example.secondapplication.R
 import com.example.secondapplication.objects.Item
+import com.bumptech.glide.request.target.Target
+
 
 class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     protected val image = view.findViewById<ImageView>(R.id.image)
 
-    protected val imageLoader by lazy { Picasso.get() }
-
     fun bind(item: Item) {
         val outer = this
-        imageLoader.load(item.imageValue()).into(image, object : Callback {
-            override fun onSuccess() {
-                itemView.findViewById<ProgressBar>(R.id.progressBar_cyclic).visibility =
-                    INVISIBLE
-                itemView.findViewById<Button>(R.id.reload_button).visibility = INVISIBLE
-            }
-
-            override fun onError(e: Exception?) {
+        Glide.with(itemView).load(item.imageValue()).listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
                 var progressBar_cyclic = itemView.findViewById<ProgressBar>(R.id.progressBar_cyclic)
                 progressBar_cyclic.visibility = INVISIBLE
                 var button = itemView.findViewById<Button>(R.id.reload_button)
@@ -36,7 +38,25 @@ class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     progressBar_cyclic.visibility = VISIBLE
                     outer.bind(item)
                 }
+
+                return false
             }
-        })
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+
+                itemView.findViewById<ProgressBar>(R.id.progressBar_cyclic).visibility =
+                    INVISIBLE
+                itemView.findViewById<Button>(R.id.reload_button).visibility = INVISIBLE
+
+                return false
+            }
+
+        }).into(image)
     }
 }
